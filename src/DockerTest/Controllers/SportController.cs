4dd881a39eaf7241e0fcdae83bc96b20b040ad88
Dave Martin <dmartin@sportingindex.com>
2015-09-10 15:41:29 +0100

    initial commit

---
+++ src/DockerTest/Controllers/SportController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DockerTest.Controllers
+{
+    [Route("api/[controller]")]
+    public class SportController : Controller
+    {
+        private static List<string> Sports = new List<string> {"Football", "Tennis", "Rugby", "Cricket"};
+
+            // GET: api/sport
+        [HttpGet]
+        public string Get()
+        {
+            var containerNumString = Environment.GetEnvironmentVariable("HOSTNAME")?.Split('-').Last() ?? "1";
+            var containerNum = Int32.Parse(containerNumString);
+            var index = (containerNum - 1) % Sports.Count;
+            return Sports[index];
+        }
+    }
+}
---
+++ src/DockerTest/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace DockerTest.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        // GET: api/test
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
